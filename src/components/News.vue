d5307ebc918663d4308addfaf1a9f983095e5d3b
gaofei019 <gaofei019@163.com>
2017-02-08 17:10:16 +0800

    add files

---
+++ src/components/News.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="news">
+        <h3>我是新闻页</h3>
+        <ul>
+            <li v-for="(val,index) in news">
+                {{val}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default{
+        name:'news',
+        data(){
+            return {
+                news:['111','222','333','444']
+            }
+        }
+    }
+</script>
+<style scope>
+    li{ border-bottom:1px dashed #dedede;}
+</style>
